8d16abdee94a807ad5e1f3a8d0b1654dc16a5aef
Dora-fx <dhzibovska@gmail.com>
2024-06-15 18:02:46 +0300

    js added, but kruvo-koso

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
-let firstOperand = document.addEventListener()
+let display = document.querySelector('.numbWindow');
+
+let buttons = Array.from(document.querySelectorAll('.btn-style'));
+
+buttons.map((button) => {
+    button.addEventListener('click', (e) => {
+        // console.log(e.target.innerText);
+        switch(e.target.innerText) {
+            case "AC":
+                display.innerText = "0";
+                break;
+            case "=":
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch (e) {
+                    display.innerText = "Error";
+                }
+                break;
+            case "%":
+                let passedText = display.innerText + "/100";
+                display.innerText = eval(passedText);
+                break;
+            case "+/-":
+                display.innerText = "-";
+                break;
+
+            default:
+                if (display.innerText === "0" && e.target.innerText !== ".") {
+                    display.innerText = e.target.innerText;
+                } else {
+                    display.innerText += e.target.innerText;
+                }
+        }
+    })
+})
